cfea5de46e2fdf9608e1fcaef500f97db71fa94c
saiashish9 <saiashish3760@gmail.com>
2022-02-17 17:27:25 +0530

    initial commit

---
+++ TouchPad/TouchPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchPadApp.swift
+//  TouchPad
+//
+//  Created by Sai Ashish Darapureddy on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
